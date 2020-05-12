dc11dc9eed737e6dfe95a4311e5c2867bbffb577
Duc Thang Vu <vuducthang91@gmail.com>
2020-05-12 16:56:02 +0200

    completed

--- script/main.js
+++ script/main.js
@@ -1,6 +1,27 @@
-console.log('main.js is working')
-console.log('$')
+/******/ (() => { // webpackBootstrap
+/******/ 	/************************************************************************/
+(() => {
+/*!****************************!*\
+  !*** ./src/script/main.js ***!
+  \****************************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [unused] */
+/*! runtime requirements:  */
+var a = 10;
+var b = 20;
+console.log('main.js is working');
+console.log(b);
+})();
 
-$(document).ready(function () {
-    
-});
+(() => {
+/*!***************************!*\
+  !*** ./src/css/main.scss ***!
+  \***************************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [unused] */
+/*! runtime requirements:  */
+// extracted by mini-css-extract-plugin
+})();
+
+/******/ })()
+;
---
+++ src/script/main.js
@@ -0,0 +1,5 @@
+var a = 10
+var b = 20
+
+console.log('main.js is working')
+console.log(b)
